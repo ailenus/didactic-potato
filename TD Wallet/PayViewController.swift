38a58dfa80abb98f3aa47cd9256c39d33266f565
qbiqing <bq2134@columbia.edu>
2018-02-03 13:34:16 -0500

    added views viewcontrollers for pay and collect

--- TD Wallet/PayViewController.swift
+++ TD Wallet/PayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ThirdViewController.swift
+//  PayViewController.swift
 //  TD Wallet
 //
 //  Created by Biqing Qiu on 2/3/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class PayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
